4b513b059f00643113f3a3d1ee03d7e03334e1c0
victoria-kholina <kholina.victoria@gmail.com>
2017-11-04 18:10:05 +0200

    TODO: responsive, slider css, tabs.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function(){
+    
+getDropdown();  
+$(window).resize(getDropdown); 
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+      loop: false,
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+    
+      paginationBulletRender: function (swiper, index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 5
+    }); 
+
+    function getDropdown(){
+        var winWidth = $(window).width();
+        if(winWidth<='768'){ 
+            $('.main-menu button').removeClass("hidden").addClass("show");
+            $('.main-menu').addClass("dropdown btn-group");
+            $('.main-menu ul').addClass("dropdown-menu");
+        } else { 
+            $('.main-menu button').removeClass("show").addClass("hidden");
+            $('.main-menu').removeClass("dropdown");
+            $('.main-menu ul').removeClass("dropdown-menu");     
+        };
+        };
+    
+    
+        
+});//end jQuery
+                    
